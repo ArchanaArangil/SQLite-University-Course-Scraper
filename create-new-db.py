45812ab5488194ba25e7d6d11fadf1150fd3dd71
Archana Arangil <115735886+ArchanaArangil@users.noreply.github.com>
2024-06-04 01:04:19 -0500

    Update create-new-db.py

--- create-new-db.py
+++ create-new-db.py
@@ -4,8 +4,21 @@ conn = sqlite3.connect('colleges-and-courses.db')
 cursor = conn.cursor()
 
 cursor.execute('''
-            DROP TABLE IF EXISTS all_courses
-               ''')
+            CREATE TABLE IF NOT EXISTS colleges (
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               college_name TEXT NOT NULL
+            )
+        ''')
+
+cursor.execute('''
+            CREATE TABLE IF NOT EXISTS courses (
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               course_name TEXT NOT NULL,
+               degree_type TEXT,
+               college_id INTEGER
+            )
+        ''')
+
 conn.commit()
 conn.close()
 
